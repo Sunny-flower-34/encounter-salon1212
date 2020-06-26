55d8b671b1004d114ba680927f0624f8a383bbab
Sunny-flower-34 <66055547+Sunny-flower-34@users.noreply.github.com>
2020-06-26 22:53:19 +0900

    Front user details page

--- app/javascript/home.js
+++ app/javascript/home.js
@@ -1,7 +1,1 @@
-$(function() {
-  $(document).on("mouseover", "h1", function(){
-    $(this).css({"color": "blue"});
-  }).on("mouseout", "h1", function(){
-    $(this).css({"color": ""});
-  })
-});
+
