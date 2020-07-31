bf021c108e0eca4eb3d064038d7171d41e7e96e7
Sunny-flower-34 <sunnyflower8834@gmail.com>
2020-07-31 17:13:40 +0900

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,8 +2,8 @@
 lock '3.14.1'
 
 # 自身のアプリ名、リポジトリ名を記述
-set :application, 'encounter-salon'
-set :repo_url,  'git@github.com:Sunny-flower-34/encounter-salon.git'
+set :application, 'encounter-salon1212'
+set :repo_url,  'git@github.com:Sunny-flower-34/encounter-salon1212.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
