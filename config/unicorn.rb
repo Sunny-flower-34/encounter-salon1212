03bab2485f479d361d9c4620855781302e3f69cd
Sunny-flower-34 <sunnyflower8834@gmail.com>
2020-07-31 16:01:11 +0900

    deploy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -17,10 +17,10 @@ listen 3000
 # listen "#{app_path}/tmp/sockets/unicorn.sock"
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 #エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/log/unicorn.stderr.log"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 
 #通常のログを記録するファイルを指定
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
